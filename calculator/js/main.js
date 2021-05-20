b2dd7e3e0492099a860871746c0ec8dea4fc03e4
dzwager <70720747+dzedcole@users.noreply.github.com>
2021-05-20 19:00:35 +0200

    Add files via upload

---
+++ calculator/js/main.js
@@ -0,0 +1,11 @@
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operators");
+const calculate = document.querySelector(".calculate-button");
+const answer = document.querySelector(".answer");
+let firstNumber;
+let secondNumber;
+
+function inputNumber1() {
+
+}
+numbers.addEventListener("click", inputNumber1);
