82591054a5f7afbfbcb01241ea7bfba0f8f9bd02
dzedcole <302989209@student.rocmondriaan.nl>
2021-06-03 11:05:41 +0200

    .

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 function returnTo() {
     window.history.go(-1);
 }
+/* Variables */
+
+/* index */
+
+/* Over mij */
+
+/* Projecten */
+
+/* Contact */
+
+/* Functions */
 function select() {
 let audio = new Audio();
 audio.volume = "1.0";
 audio.src = "./sounds/select.mp3";
 audio.play();
+}
+function currentTheme() {
+
 }
