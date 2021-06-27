7f302668076b1ec539e40b0970f094827d7da702
dzedcole <302989209@student.rocmondriaan.nl>
2021-06-27 22:24:41 +0200

    added theme switcher, finished over mij

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ function returnTo() {
     window.history.go(-1);
 }
 /* Variables */
+let currentTheme = "main";
+
 
 /* index */
 
@@ -15,25 +17,71 @@ function returnTo() {
 function select() {
 let audio = new Audio();
 audio.volume = "1.0";
-audio.src = "./sounds/select.mp3";
+audio.src = "../sounds/select.mp3";
 audio.play();
-}
+}console.log(localStorage.getItem("current-theme"))
 function themeSwitch() {
+    let main = document.querySelector(".main");
+    let navbar = document.querySelector(".navbar");
 
+    localStorage.setItem("current-theme", currentTheme)
+    console.log(currentTheme)
+    
+
+    if (localStorage.getItem("current-theme") == "main") {
+    main.className = "main dark-mode";
+    navbar.className = "navbar nav-dark-mode";
+
+    document.querySelector("html").style.background = "url(../img/bg-night.png)";
+    document.querySelector("html").style.backgroundRepeat = "no-repeat";
+    document.querySelector("html").style.backgroundSize = "cover";
+
+    currentTheme = "dark-mode";
+    localStorage.clear()
+    localStorage.setItem("current-theme", currentTheme);
 
     select()
+    }
+    else {
+        main.className = "main";
+        navbar.className = "navbar";
+
+        document.querySelector("html").style.background = "url(../img/bg-day.png)";
+        document.querySelector("html").style.backgroundRepeat = "no-repeat";
+        document.querySelector("html").style.backgroundSize = "cover";
+
+        currentTheme = "main";
+        localStorage.clear()
+        localStorage.setItem("current-theme", currentTheme);
+        
+        select()
+    }
+
+    
 }
     let d = new Date;
     let n = d.getHours()
-    console.log(n)
-    if (n < 19 || n > 7) {
-        document.getElementById("html").style.background = "url(../img/bg-day.png)";
-        document.getElementById("html").style.backgroundRepeat = "no-repeat";
-        document.getElementById("html").style.backgroundSize = "cover";
+
+    if (n > 7) {
+        if (n < 19 ) {
+            document.querySelector("html").style.background = "url(../img/bg-day.png)";
+            document.querySelector("html").style.backgroundRepeat = "no-repeat";
+            document.querySelector("html").style.backgroundSize = "cover";
+
+            currentTheme = "main";
+            localStorage.setItem("current-theme", currentTheme);
+            console.log (currentTheme)
     }
-    else {
-        document.getElementById("html").style.background = "url(../img/bg-night.png)";
-        document.getElementById("html").style.backgroundRepeat = "no-repeat";
-        document.getElementById("html").style.backgroundSize = "cover";
+}
+    else {            
+            document.querySelector("html").style.background = "url(../img/bg-night.png)";
+            document.querySelector("html").style.backgroundRepeat = "no-repeat";
+            document.querySelector("html").style.backgroundSize = "cover";
+
+            currentTheme = "dark-mode";
+            localStorage.setItem("current-theme", currentTheme);
+            console.log(currentTheme)
     }
+
+localStorage.clear()
 console.log("File Loaded!")
